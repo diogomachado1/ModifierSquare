034ef6036155ff1507b80ff5d53f0d2de3fe24ab
Diogo Machado <diogo8machado@gmail.com>
2019-01-30 17:08:35 -0300

    Melhorias

--- dist/js/Trans.js
+++ dist/js/Trans.js
@@ -3,22 +3,46 @@ new Vue({
     data: {
         br: '0%',
     },
-    methods: {
+    mounted: function () {
+        this.$nextTick(function () {
+           setInterval(()=>{
+            this.randomizer('bola')
+           },1000)
+            
+        })
+      },
+      methods: {
         trans(){
-            let toggle = false
-            let n = 0
-            setInterval(() => {
-                document.getElementById(1).style.borderRadius=`${n}%`
-                console.log(n)
-                if(n == 100 || n==-1){
-                    toggle = !toggle
+          this.randomizer('bola')
+        },
+        randomizer(id) {
+
+                let color='#'
+                for(let i = 0;i < 6;i++){
+                    color+=this.hexadecimal()
                 }
-                if(!toggle){
-                    n++
-                }else{
-                    n--
-                }  
-            }, 20)
+                document.getElementById(id).style.backgroundColor = color
+        },
+        hexadecimal(){
+            let r = Math.floor(Math.random() *16)//0 1 2 3 4 5 6 7 8 9 A B C D E F
+            switch (r) {
+                case 10:
+                    return 'A'
+                case 11:
+                    return 'B'
+                case 12:
+                    return 'C'
+                case 13:
+                    return 'D'
+                case 14:
+                    return 'E'
+                case 15:
+                    return 'F'
+        
+                default:
+                    return r.toString()
+            }
+        
         }
-    },
+    }
 })
