08a85ece8a9e6eee7d59013c4eae313cb3f1ad70
Maicon Douglas <maicondouglas75@hotmail.com>
2019-01-30 15:59:42 -0200

    First Commit

---
+++ dist/js/Trans.js
@@ -0,0 +1,24 @@
+new Vue({
+    el: "#app",
+    data: {
+        br: '0%',
+    },
+    methods: {
+        trans(){
+            let toggle = false
+            let n = 0
+            setInterval(() => {
+                document.getElementById(1).style.borderRadius=`${n}%`
+                console.log(n)
+                if(n == 100 || n==-1){
+                    toggle = !toggle
+                }
+                if(!toggle){
+                    n++
+                }else{
+                    n--
+                }                
+            }, 20)
+        }
+    },
+})
